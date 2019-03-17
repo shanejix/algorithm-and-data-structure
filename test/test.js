a82d8ecc8693b47aadc0afc86187c9f99446cfcc
xian ji <shanejix@vip.163.com>
2019-03-17 23:56:07 +0800

    arr

--- test/test.js
+++ test/test.js
@@ -1,6 +1,11 @@
 import reverseworlds from '../leetcode/string/557. Reverse Worlds In String/reverseworlds';
 import reverseworlds2 from '../leetcode/string/557. Reverse Worlds In String/reverseworlds2';
+import letterCombin from '../leetcode/array/17. Letter Combinations of a Phone Number/letterCombinations'
 
-test('reverseworlds', () => {
-  expect(reverseworlds("Let's take LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc");
+// test('reverseworlds', () => {
+//   expect(reverseworlds("Let's take LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc");
+// });
+
+test('letterCombin', () => {
+  expect(letterCombin("23")).toBe(["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]);
 });
