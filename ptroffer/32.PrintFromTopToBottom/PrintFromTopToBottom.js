1ad7b9083e6fceb898378355c5585c754e4893bd
xian ji <shanejix@vip.163.com>
2019-05-02 19:02:44 +0800

    32

--- ptroffer/32.PrintFromTopToBottom/PrintFromTopToBottom.js
+++ ptroffer/32.PrintFromTopToBottom/PrintFromTopToBottom.js
@@ -6,7 +6,26 @@
     this.left = null;
     this.right = null;
 } */
+
+//思路：层次遍历
+
 function PrintFromTopToBottom(root)
 {
-    // write code here
+    if (!root) {
+        return
+    }
+    
+    let queue = [];
+    queue.push(root);
+
+    while (queue.length) {
+        let node = queue.shift();
+        console.log(node.val);
+        if (node.left) {
+            queue.push(node.left);
+        }
+        if (node.right) {
+            queue.push(node.right);
+        }
+    }
 }
