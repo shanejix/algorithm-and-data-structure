9365d2ad60ed64666ed28b19f24cf489688b3be8
xian ji <shanejix@vip.163.com>
2019-05-03 14:30:19 +0800

    33

--- ptroffer/33.VerifySquenceOfBST/VerifySquenceOfBST.js
+++ ptroffer/33.VerifySquenceOfBST/VerifySquenceOfBST.js
@@ -4,5 +4,34 @@
 
 function VerifySquenceOfBST(sequence)
 {
-    // write code here
+    if (!sequence || sequence.length == 0) {
+        return false;
+    }
+
+    let len = sequence.length;
+    let root = sequence[len - 1];
+
+    for (let i = 0; i < len - 1; i++){
+        if (sequence[i] > root) {
+            break;
+        }
+    }
+
+    for (let j = i; j < len - 1; j++){
+        if (sequence[j] < root) {
+            return false;
+        }
+    }
+
+    let left = true;
+    if (i > 0) {
+        left = VerifySquenceOfBST(sequence.slice(0, i));
+    }
+
+    let right = true;
+    if (j < len - 1) {
+        right = VerifySquenceOfBST(sequence.slice(i));
+    }
+
+    return left && right
 }
