6bb0febcb14790374691d8d4a3dbcb0dd13d87a7
xian ji <shanejix@vip.163.com>
2019-05-06 20:29:04 +0800

    37

--- ptroffer/37.Serialize/Serialize.js
+++ ptroffer/37.Serialize/Serialize.js
@@ -1,16 +1,35 @@
 // 题目描述
 // 请实现两个函数，分别用来序列化和反序列化二叉树
 
-/* function TreeNode(x) {
+function TreeNode(x) {
     this.val = x;
     this.left = null;
     this.right = null;
-} */
-function Serialize(pRoot)
-{
-    // write code here
 }
-function Deserialize(s)
-{
-    // write code here
+
+let arr = [];
+
+function Serialize(pRoot) {
+    if (!pRoot) {
+        arr.push('$');
+    }
+    else {
+        arr.push(pRoot.val);
+        Serialize(pRoot.left);
+        Serialize(pRoot.right);
+    }
+}
+function Deserialize(s) {
+    let node = null;
+    if (arr.length < 1) {
+        return null;
+    }
+
+    let num = arr.shift();
+    if (typeof num === 'number') {
+        node = new TreeNode(num);
+        node.left = Deserialize(arr);
+        node.right = Deserialize(arr);
+    }
+    return node;
 }
