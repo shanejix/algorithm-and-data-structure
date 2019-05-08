1850403db5277bc7f02a06fdc7d0ca4bb9b61adb
xian ji <shanejix@vip.163.com>
2019-05-08 20:08:50 +0800

    39

--- ptroffer/39.MoreThanHalfNum_Solution/MoreThanHalfNum_Solution.js
+++ ptroffer/39.MoreThanHalfNum_Solution/MoreThanHalfNum_Solution.js
@@ -4,7 +4,33 @@
 // 由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。
 // 如果不存在则输出0。
 
+//思路：
+
+//遍历arr，统计每个字母出现的次数且记录位置
+
 function MoreThanHalfNum_Solution(numbers)
 {
-    // write code here
+    let count = {};
+    numbers.forEach(val => {
+        if (count[val]) {
+            count[val]++;
+        } else {
+            count[val] = 1;
+        }
+    });
+
+    let max = 0;//Number.MIN_SAFE_INTEGER
+    let letter;
+    for (i in count) {
+        if (count[i] > max) {//hasOwnProperty
+            max = count[i];
+            letter = i;
+        }
+    }
+
+    if (max > Math.floor(numbers.length / 2)) {//>>1
+        return letter;
+    } else {
+        return 0;
+    }
 }
