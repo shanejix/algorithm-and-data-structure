3f2c12da57fda2736b91ca838071339482c9401c
xian ji <shanejix@vip.163.com>
2019-05-12 09:31:15 +0800

    43

--- ptroffer/43.NumberOf1Between1AndN_Solution/NumberOf1Between1AndN_Solution.js
+++ ptroffer/43.NumberOf1Between1AndN_Solution/NumberOf1Between1AndN_Solution.js
@@ -5,7 +5,26 @@
 // 但是对于后面问题他就没辙了。ACMer希望你们帮帮他, 并把问题更加普遍化,
 // 可以很快的求出任意非负整数区间中1出现的次数（从1 到 n 中1出现的次数）。
 
+//思路：直观的解法
+
 function NumberOf1Between1AndN_Solution(n)
 {
-    // write code here
+    let num = 0;
+
+    for (let i = 1; i <= n; i++){
+        num += numberOf(i);
+    }
+
+    return num;
+}
+
+function numberOf(number) {
+    let count = 0;
+    while (number > 0) {
+        if (number % 10 == 1) {
+            count++;
+        }
+        number = number / 10;
+    }
+    return count;
 }
