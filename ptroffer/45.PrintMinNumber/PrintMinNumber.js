03b2b8273d820a4c769cf487ae89c523574f6030
xian ji <shanejix@vip.163.com>
2019-05-14 10:06:11 +0800

    45

--- ptroffer/45.PrintMinNumber/PrintMinNumber.js
+++ ptroffer/45.PrintMinNumber/PrintMinNumber.js
@@ -2,7 +2,22 @@
 // 输入一个正整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
 // 例如输入数组{ 3，32，321 } ，则打印出这三个数字能排成的最小数字为321323。
 
+
+//思路一：先排序
+
 function PrintMinNumber(numbers)
 {
-    // write code here
+    numbers.sort(function(a,b){
+        var s1=a+''+b;
+        var s2=b+''+a;
+        return s1-s2;
+    })
+    var result="";
+    numbers.map(function(num){
+        result=result.concat(num)
+    })
+    return result;
 }
+
+
+//思路二：剑指offer
