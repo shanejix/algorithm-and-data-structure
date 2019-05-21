c21bd9f954f9e5c91c9e8cf6eef795699f3790c6
xian ji <shanejix@vip.163.com>
2019-05-21 22:06:28 +0800

    51

--- ptroffer/51.InversePairs/InversePairs.js
+++ ptroffer/51.InversePairs/InversePairs.js
@@ -14,7 +14,43 @@
 
 // 	对于%100的数据,size<=2*10^5
 
-function InversePairs(data)
-{
-    // write code here
+
+
+
+function InversePairs(data) {
+    if (!data || data.length < 2) return 0;
+
+    var copy = data.slice(),
+        count = 0;
+    count = mergeSort(data, copy, 0, data.length - 1);
+    return count % 1000000007;
+}
+
+function mergeSort(data, copy, start, end) {
+    if (end === start) return 0;
+    var mid = (end - start) >> 1,
+        left = mergeSort(copy, data, start, start + mid),
+        right = mergeSort(copy, data, start + mid + 1, end),
+        count = 0,
+        p = start + mid,//前一个数组的最后一个下标
+        q = end,//后一个数组的下标
+        copyIndex = end;//辅助数组下标，从最后一个算起
+
+    while (p >= start && q >= start + mid + 1) {
+        if (data[p] > data[q]) {
+            count += q - start - mid;
+            copy[copyIndex--] = data[p--];
+        } else {
+            copy[copyIndex--] = data[q--];
+        }
+    }
+
+    while (p >= start) {
+        copy[copyIndex--] = data[p--];
+    }
+
+    while (q >= start + mid + 1) {
+        copy[copyIndex--] = data[q--];
+    }
+    return left + right + count;
 }
