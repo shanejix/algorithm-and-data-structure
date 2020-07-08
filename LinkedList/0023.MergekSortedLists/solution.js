5b39380373f35581d5d23476b849c097e0efa7d8
shane <shanejix@vip.163.com>
2020-07-08 22:14:08 +0800

    add 0023

--- LinkedList/0023.MergekSortedLists/solution.js
+++ LinkedList/0023.MergekSortedLists/solution.js
@@ -21,5 +21,44 @@ function ListNode(val, next) {
  * @return {ListNode}
  */
 var mergeKLists = function (lists) {
-    
-};  
+  return mergeList(lists, 0, lists.length - 1);
+};
+
+function mergeList(listNodes, begin, end) {
+  if (begin > end) {
+    return null;
+  }
+
+  if (begin === end) return listNodes[begin];
+
+  let mid = (begin + end) >> 1;
+
+  let listNode1 = mergeList(listNodes, begin, mid);
+  let listNode2 = mergeList(listNodes, mid + 1, end);
+
+  let merges = merge(listNode1, listNode2);
+
+  return merges;
+}
+
+function merge(l1, l2) {
+  let p1 = l1;
+  let p2 = l2;
+  let result = new ListNode(null);
+  let p3 = result;
+
+  while (p1 && p2) {
+    if (p1.val > p2.val) {
+      p3.next = p2;
+      p2 = p2.next;
+    } else {
+      p3.next = p1;
+      p1 = p1.next;
+    }
+    p3 = p3.next;
+  }
+
+  p3.next = p1 || p2;
+
+  return result.next;
+}
