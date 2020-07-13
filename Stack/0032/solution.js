9ddb3e662343e056be2b9b586f8fcc9083ffe402
shane <shanejix@vip.163.com>
2020-07-13 20:58:53 +0800

    add 0032

---
+++ Stack/0032/solution.js
@@ -0,0 +1,24 @@
+function _(s) {
+  let longest = 0;
+  const stack = [];
+
+  if (!s) return 0;
+
+  // init -1
+  stack.push(-1);
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      //  store item index
+      stack.push(i);
+    } else {
+      // in case :item === ) and inital stack -1
+      stack.pop();
+      if (stack.length === 0) {
+        stack.push(i);
+      } else {
+        longest = Math(longest, i - stack[stack.length - 1]);
+      }
+    }
+  }
+}
