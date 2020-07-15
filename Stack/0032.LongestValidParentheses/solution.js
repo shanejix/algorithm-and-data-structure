bb856821eeeca678fefd0cf38e4a9bc1e3cc70e0
shane <shanejix@vip.163.com>
2020-07-15 22:37:52 +0800

    update 0032

--- Stack/0032.LongestValidParentheses/solution.js
+++ Stack/0032.LongestValidParentheses/solution.js
@@ -1,3 +1,17 @@
+// Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+// Example 1:
+
+// Input: "(()"
+// Output: 2
+// Explanation: The longest valid parentheses substring is "()"
+
+// Example 2:
+
+// Input: ")()())"
+// Output: 4
+// Explanation: The longest valid parentheses substring is "()()"
+
 function _(s) {
   let longest = 0;
   const stack = [];
