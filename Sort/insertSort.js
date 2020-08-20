738d5cc3373efaa258203b2ad65c1144587567dd
shanejixx <shanejix@vip.163.com>
2020-08-20 20:20:03 +0800

    Create insertSort.js

---
+++ Sort/insertSort.js
@@ -0,0 +1,25 @@
+function insertSort(arr) {
+  if (!arr) return;
+
+  // 循环轮次
+  for (let i = 1; i < arr.length; i++) {
+    // 待插数据
+    let value = arr[i];
+    // 每一轮从待排序区间向已排序区间插入
+    for (let j = i; j > 0; j--) {
+      // 升序
+      if (arr[j - 1] > value) {
+        arr[j] = arr[j - 1];
+        arr[j - 1] = value;
+      } else {
+        break;
+      }
+    }
+  }
+
+  return arr;
+}
+
+let testArr = [9, 8, 7, 6, 5, 4, 3, 2, 1];
+
+console.log(insertSort(testArr));
