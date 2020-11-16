de20fbd1ce93c382d207df449013492acce6d37c
shane <shanejix@vip.163.com>
2020-11-16 22:00:47 +0800

    Create ArrayList.interface.js

---
+++ ArrayList/ArrayList.interface.js
@@ -0,0 +1,81 @@
+class ArrayList {
+  constructor(capaticy) {
+    this.elements = new Array(capaticy);
+    this.size = 0;
+  }
+
+  /**
+   * 清除所有元素
+   * @return
+   */
+  clear() {}
+
+  /**
+   * 元素的数量
+   * @return number
+   */
+  size() {}
+
+  /**
+   * 是否为空
+   * @return boolean
+   */
+  isEmpty() {}
+
+  /**
+   * 是否包含某个元素
+   * @param element
+   * @return boolean
+   */
+  contains(element) {}
+
+  /**
+   * 添加元素到尾部
+   * @param element
+   */
+  add(element) {}
+
+  /**
+   * 获取index位置的元素
+   * @param index
+   * @return number
+   */
+  get(index) {}
+
+  /**
+   * 设置index位置的元素
+   * @param index
+   * @param element
+   * @return element
+   */
+  set(index, element) {}
+
+  /**
+   * 在index位置插入一个元素
+   * @param index
+   * @param element
+   * @return
+   */
+  insert(index, element) {}
+
+  /**
+   * 删除index位置的元素
+   * @param index
+   * @return element
+   */
+  remove(element) {}
+
+  /**
+   * 查看元素的索引
+   * @param element
+   * @return number
+   */
+  indexOf(element) {}
+
+  /**
+   * 打印元素
+   * @param element
+   * @return element
+   */
+  console() {}
+}
