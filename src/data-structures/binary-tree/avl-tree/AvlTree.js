2c453b62f7d06523f47604a71a742ac01477d749
shanejixx <shanejixx@outlook.com>
2020-12-21 17:50:33 +0800

    :hammer: update

--- src/data-structures/binary-tree/avl-tree/AvlTree.js
+++ src/data-structures/binary-tree/avl-tree/AvlTree.js
@@ -1,3 +1,41 @@
-export default class AvlTree {
+import BinarySearchTree from '../binary-search-tree/BinarySearchTree';
+
+export default class AvlTree extends BinarySearchTree{
+  /**
+   * @param {*} value
+   * @return {*}
+   */
+  insert(value){
+    // BinarySearchTree.insert
+    super.insert(value);
+
+    // move up from current node to root to check balance factors
+    let currentNode = this.root.find(value);
+    while (currentNode){
+      this.balance(currentNode);
+      currentNode = currentNode.parent;
+    }
+  }
+
+  /**
+   * @param {*} value
+   * @return {*}
+   */
+  remove(value){
+    // BinarySearchTree.remove
+    super.remove(value);
+
+    // 
+    this.balance(this.root)
+
+  }
+
+  /**
+   * @param {*} node
+   * @return {*}
+   */
+  balance(node){
+
+  }
 
 }
