b8cbad7031e9eda8bbb279077556e72ac1c7f9b0
shanejixx <shanejixx@outlook.com>
2020-12-31 22:29:03 +0800

    :hammer: add red black tree

---
+++ src/data-structures/binary-tree/red-black-tree/RedBlackTree.js
@@ -0,0 +1,15 @@
+import BinarySearchTree from "../binary-search-tree/BinarySearchTree";
+
+export default class RedBlackTree extends BinarySearchTree {
+  /**
+   *
+   * @param {*} value
+   */
+  insert(value) {}
+
+  /**
+   *
+   * @param {*} value
+   */
+  remove(value) {}
+}
