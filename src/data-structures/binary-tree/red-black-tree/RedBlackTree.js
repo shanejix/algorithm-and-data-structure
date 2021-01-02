7d374ae625c9e53a66526bb59afdbbec6e9cb71a
shanejixx <shanejixx@outlook.com>
2021-01-02 15:10:13 +0800

    :hammer: update red black tree insert methed

--- src/data-structures/binary-tree/red-black-tree/RedBlackTree.js
+++ src/data-structures/binary-tree/red-black-tree/RedBlackTree.js
@@ -1,15 +1,39 @@
 import BinarySearchTree from "../binary-search-tree/BinarySearchTree";
 
 export default class RedBlackTree extends BinarySearchTree {
+
   /**
    *
+   *
    * @param {*} value
+   * @returns {*}  
    */
-  insert(value) {}
+  insert(value) {
+    const insertedNode = super.insert(value);
+
+    if (this.nodeComparator.equal(insertedNode, this.rootNode)) {
+      // make root always be black
+      this.makeNodeBlack(insertedNode);
+    } else {
+      // make all newly inserted nodes to be red
+      this.makeNodeRed(insertedNode);
+    }
+
+    // check all conditions and balance the nodes
+    this.balance();
+
+    return insertedNode;
+  }
 
   /**
    *
    * @param {*} value
    */
-  remove(value) {}
+  remove(value) { }
+
+  balance(node) { }
+
+  makeNodeRed(node) { }
+
+  makeNodeBlack(node) { }
 }
