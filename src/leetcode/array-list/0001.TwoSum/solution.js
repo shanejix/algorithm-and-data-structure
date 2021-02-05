f70ad456e0c6ad78465da7aef93d17e8988f53a3
shanejix <shanejix@hotmail.com>
2021-02-05 18:12:47 +0800

    📝 udpate 0001

--- src/leetcode/array-list/0001.TwoSum/solution.js
+++ src/leetcode/array-list/0001.TwoSum/solution.js
@@ -30,20 +30,20 @@ function twoSum(nums, target) {
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+var twoSum1 = function (nums, target) {
 
-  if(!nums){
-      return
+  if (!nums) {
+    return
   }
 
-  for(let i = 0;i<nums.length;i++){
-      let curr = nums[i]
-      let minu  = target - curr;
-      let tIdx = nums.indexOf(minu,i+1);
+  for (let i = 0; i < nums.length; i++) {
+    let curr = nums[i]
+    let minu = target - curr;
+    let tIdx = nums.indexOf(minu, i + 1);
 
-      if(tIdx !== -1){
-          return [i,tIdx];
-      }
+    if (tIdx !== -1) {
+      return [i, tIdx];
+    }
   }
 };
 
@@ -85,6 +85,34 @@ function twoSum3(nums, target) {
   }
 }
 
-console.log("brute-loop", twoSum([1, 7, 2, 8], 9));
-console.log("for-loop", twoSum2([1, 7, 2, 8], 9));
-console.log("for-in loop", twoSum2([1, 7, 2, 8], 9));
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum4 = function (nums, target) {
+
+  if (!nums) {
+    return
+  }
+
+  let map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+
+    let curr = nums[i];
+    let minu = target - curr;
+    let tIdx = map[minu];
+
+    if (tIdx !== undefined) {
+      return [i, tIdx];
+    }
+
+    map[curr] = i;
+
+  }
+};
+// console.log("brute-loop", twoSum([1, 7, 2, 8], 9));
+// console.log("for-loop", twoSum2([1, 7, 2, 8], 9));
+// console.log("for-in loop", twoSum2([1, 7, 2, 8], 9));
+console.log("twoSum4", twoSum4([2, 7, 11, 15], 9));
