9573ef4add710eaf06602817de3d07af810b2299
shanejix <shanejix@hotmail.com>
2021-02-18 11:04:00 +0800

    ⚡️ udpate

--- src/leetcode/linked-list/0160.IntersectionofTwoLinkedLists/solution.js
+++ src/leetcode/linked-list/0160.IntersectionofTwoLinkedLists/solution.js
@@ -99,3 +99,33 @@ function listLenth(head) {
 
     return len;
 }
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNodeOpitmization = function (headA, headB) {
+    if (!headA || !headB) {
+        return null;
+    }
+
+    let currA = headA;
+    let currB = headB;
+
+    while (currA !== currB) {
+        currA = currA === null ? headB : currA.next;
+        currB = currB === null ? headA : currB.next;
+    }
+
+    return currA;
+};
