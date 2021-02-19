28360aaacaff228ded7b9b738e3fd47ee414d1b0
shanejix <shanejix@hotmail.com>
2021-02-19 15:46:46 +0800

    ⚡️ uddate

--- src/leetcode/linked-list/0021.MergeTwoSortedLists/solution.js
+++ src/leetcode/linked-list/0021.MergeTwoSortedLists/solution.js
@@ -85,3 +85,34 @@ var mergeTwoListsOptimization = function (l1, l2) {
   return r.next;
 
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+// 递归解法
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+
+  if (l1 === null) {
+    return l2
+  } else if (l2 === null) {
+    return l1;
+  } else if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l2.next, l1);
+    return l2;
+  }
+
+};
