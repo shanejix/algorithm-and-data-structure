60d1caa5bf340cba52b7971d0ee5b6ea2eba6ff4
shanejix <shanejix@hotmail.com>
2021-02-19 16:54:07 +0800

    📝 udpate 0234

---
+++ src/leetcode/linked-list/0234.PalindromeLinkedList/solution.js
@@ -0,0 +1,53 @@
+// Given a singly linked list, determine if it is a palindrome.
+
+// Example 1:
+
+// Input: 1->2
+// Output: false
+// Example 2:
+
+// Input: 1->2->2->1
+// Output: true
+// Follow up:
+// Could you do it in O(n) time and O(1) space?
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+// 利用回文链表的对称性
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+    let newHead = revert(head);
+
+    while (head && newHead) {
+
+        if (head.val !== newHead.val) {
+            return false;
+        }
+        head = head.next;
+        newHead = newHead.next;
+    }
+
+    return true
+};
+
+function revert(head) {
+    let newHead = new ListNode(0);
+    let curr = head;
+    while (curr) {
+        let insert = new ListNode(curr.val);
+        insert.next = newHead.next;
+        newHead.next = insert;
+        curr = curr.next;
+    }
+    return newHead.next;
+}
