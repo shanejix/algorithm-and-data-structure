248e541ffbaeab9b65084e46770395f3d72b1dc2
shanejix <shanejix@hotmail.com>
2021-02-20 16:28:16 +0800

    ⚡️ update

--- src/leetcode/stack/0155.MinStack/solution.js
+++ src/leetcode/stack/0155.MinStack/solution.js
@@ -105,6 +105,65 @@ MinStack.prototype.getMin = function () {
     return this.minStack[this.minStack.length - 1];
 };
 
+
+
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(x)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
+
+/**
+* initialize your data structure here.
+*/
+var MinStack = function () {
+    this.stack = [];
+    this.min = Infinity;
+};
+
+// 单栈-最小值法
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function (x) {
+
+    if (x <= this.min) {
+        this.stack.push(this.min);
+        this.min = x;
+    }
+
+    this.stack.push(x);
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function () {
+    if (this.stack.pop() === this.min) {
+        this.min = this.stack.pop();
+    }
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function () {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function () {
+    return this.min;
+};
+
 /**
  * Your MinStack object will be instantiated and called as such:
  * var obj = new MinStack()
