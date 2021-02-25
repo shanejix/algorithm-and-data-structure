fb101328f6a40037835f00102b0b6b3a0ca7a6c1
shanejix <shanejix@hotmail.com>
2021-02-25 20:22:25 +0800

    ⚡️ update

--- src/leetcode/recuservie/0070.ClimbingStairs/solution.js
+++ src/leetcode/recuservie/0070.ClimbingStairs/solution.js
@@ -43,3 +43,37 @@ console.log(climbStairs(1))
 console.log(climbStairs(2))
 console.log(climbStairs(3))
 console.log(climbStairs(40))
+
+
+// 方法一：递归法
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+    if (n === 1) return 1;
+    if (n === 2) return 2;
+
+    return climbStairs(n - 1) + climbStairs(n - 2);
+};
+
+// 方法二：斐波拉契，迭代法
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+
+    let dp = [];
+
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2]
+    }
+
+    return dp[n];
+};
