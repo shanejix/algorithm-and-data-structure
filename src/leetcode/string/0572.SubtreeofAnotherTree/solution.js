865b63b76e9b3b95be72f43a61a93bb02715b4cc
shanejix <shanejix@hotmail.com>
2021-02-27 14:24:54 +0800

    📝 update 0572

---
+++ src/leetcode/string/0572.SubtreeofAnotherTree/solution.js
@@ -0,0 +1,60 @@
+// Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
+
+// Example 1:
+// Given tree s:
+
+//      3
+//     / \
+//    4   5
+//   / \
+//  1   2
+// Given tree t:
+//    4 
+//   / \
+//  1   2
+// Return true, because t has the same structure and node values with a subtree of s.
+//  
+
+// Example 2:
+// Given tree s:
+
+//      3
+//     / \
+//    4   5
+//   / \
+//  1   2
+//     /
+//    0
+// Given tree t:
+//    4
+//   / \
+//  1   2
+// Return false.
+//  
+
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+// 方法一：二叉树序列化（递归）
+
+/**
+ * @param {TreeNode} s
+ * @param {TreeNode} t
+ * @return {boolean}
+ */
+var isSubtree = function (s, t) {
+    return toString(s).includes(toString(t))
+};
+
+function toString(t) {
+    if (!t) return '#!';
+    return toString(t.left) + toString(t.right) + t.val + '!'
+}
