d3d4640c609c8cf1823c83de82d12e1e0d9c508a
shanejix <shanejix@hotmail.com>
2021-02-27 15:21:29 +0800

    ⚡️ update

--- src/leetcode/string/0242.ValidAnagram/solution.js
+++ src/leetcode/string/0242.ValidAnagram/solution.js
@@ -66,3 +66,38 @@ var isAnagram = function (s, t) {
 
     return true
 };
+
+// 方法二：哈希table法 优化
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+    if (s.length !== t.length) return false
+
+    const hash = {};
+
+    for (let i = 0; i < s.length; i++) {
+        console.log(s[i])
+        if (hash[s[i]]) {
+            hash[s[i]] = hash[s[i]] + 1
+        } else {
+            hash[s[i]] = 1;
+        }
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        if (hash[t[i]]) {
+            hash[t[i]] = hash[t[i]] - 1
+            if (hash[s[i]] < 0) {
+                return false
+            }
+        } else {
+            return false
+        }
+    }
+
+    return true
+};
