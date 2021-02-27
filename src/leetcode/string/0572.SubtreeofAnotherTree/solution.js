aacfea2e1e7a9eabd00d619980f61ad85f85acfa
shanejix <shanejix@hotmail.com>
2021-02-27 14:45:45 +0800

    📝 udpate

--- src/leetcode/string/0572.SubtreeofAnotherTree/solution.js
+++ src/leetcode/string/0572.SubtreeofAnotherTree/solution.js
@@ -80,3 +80,41 @@ var isSubtree = function (s, t) {
     return (JSON.stringify(s).indexOf(JSON.stringify(t))) > -1 ? true : false
 };
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+// 方法三：逐个比较
+
+/**
+ * @param {TreeNode} s
+ * @param {TreeNode} t
+ * @return {boolean}
+ */
+var isSubtree = function (s, t) {
+    if (s === null) return false
+
+    if (isSameTree(s, t)) {
+        return true
+    }
+
+    return isSubtree(s.left, t) || isSubtree(s.right, t);
+};
+
+function isSameTree(s, t) {
+    if (s === null && t === null) {
+        return true
+    }
+
+    if (s === null || t === null) {
+        return false
+    }
+
+    return s.val === t.val && isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+}
+
