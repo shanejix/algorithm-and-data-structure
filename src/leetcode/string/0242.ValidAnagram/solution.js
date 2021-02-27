97508f1d6693cc8ae459a983cec9918842255e57
shanejix <shanejix@hotmail.com>
2021-02-27 15:20:52 +0800

    ⚡️ update

--- src/leetcode/string/0242.ValidAnagram/solution.js
+++ src/leetcode/string/0242.ValidAnagram/solution.js
@@ -28,3 +28,41 @@ var isAnagram = function (s, t) {
     return s.split('').sort().join('') === t.split('').sort().join('')
 };
 
+
+// 方法二：哈希table法
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+    if (s.length !== t.length) return false
+
+    const hash = {};
+
+    for (let i = 0; i < s.length; i++) {
+        console.log(s[i])
+        if (hash[s[i]]) {
+            hash[s[i]] = hash[s[i]] + 1
+        } else {
+            hash[s[i]] = 1;
+        }
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        if (hash[t[i]]) {
+            hash[t[i]] = hash[t[i]] - 1
+        } else {
+            return false
+        }
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        if (hash[s[i]] < 0) {
+            return false
+        }
+    }
+
+    return true
+};
