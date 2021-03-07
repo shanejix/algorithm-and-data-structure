2e35b032b848b17c5050ad7d8e963920bfc8a3e6
shanejix <shanejix@hotmail.com>
2021-03-07 21:34:52 +0800

    ⚡️ update

--- src/leetcode/math/0204.CountPrimes/solution.js
+++ src/leetcode/math/0204.CountPrimes/solution.js
@@ -48,3 +48,34 @@ function isPrime(num) {
 
     return true;
 }
+
+
+// 方法一：循环 - 优化
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function (n) {
+    let count = 0;
+
+    for (let i = 2; i < n; i++) {
+        if (isPrime(i)) {
+            count++;
+        }
+    }
+
+    return count;
+};
+
+function isPrime(num) {
+    let max = parseInt(Math.sqrt(num));
+
+    for (let i = 2; i <= max; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
