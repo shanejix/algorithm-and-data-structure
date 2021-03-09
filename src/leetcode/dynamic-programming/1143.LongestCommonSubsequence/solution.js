83a0a51602e245da51cb319998a1e1ffd400fff3
shanejix <shanejix@hotmail.com>
2021-03-09 20:16:15 +0800

    🐛 update

--- src/leetcode/dynamic-programming/1143.LongestCommonSubsequence/solution.js
+++ src/leetcode/dynamic-programming/1143.LongestCommonSubsequence/solution.js
@@ -80,7 +80,7 @@ var longestCommonSubsequence = function (text1, text2) {
  * @param {string} text2
  * @return {number}
  */
- var longestCommonSubsequence = function (text1, text2) {
+var longestCommonSubsequence = function (text1, text2) {
     let n = text1.length;
     let m = text2.length;
 
@@ -94,17 +94,20 @@ var longestCommonSubsequence = function (text1, text2) {
 
     console.log(dp)
 
-    for (let i = 1; i <= n; i++) {
+    for (let i = 0; i < n + 1; i++) {
         dp[i][0] = 0;
     }
 
-    for (let i = 1; i <= m; i++) {
+    for (let i = 0; i < m + 1; i++) {
         dp[0][i] = 0;
     }
 
-    for (let i = 1; i <= n; i++) {
-        for (let j = 1; j <= m; j++) {
-            if (text1[i] === text2[j]) {
+    for (let i = 1; i < n + 1; i++) {
+        for (let j = 1; j < m + 1; j++) {
+
+            console.log(text1[i - 1], text2[j - 1]);
+
+            if (text1[i - 1] === text2[j - 1]) {
                 dp[i][j] = dp[i - 1][j - 1] + 1;
             } else {
                 dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
