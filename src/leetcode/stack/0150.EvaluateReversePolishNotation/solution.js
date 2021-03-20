eeda2b5356c5e5f0e368fe3cb31b964f8cc33a1f
shanejix <shanejix@hotmail.com>
2021-03-20 20:46:35 +0800

    📝 update 0150

---
+++ src/leetcode/stack/0150.EvaluateReversePolishNotation/solution.js
@@ -0,0 +1,79 @@
+// Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+// Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+
+// Note that division between two integers should truncate toward zero.
+
+// It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate to a result, and there will not be any division by zero operation.
+
+//  
+
+// Example 1:
+
+// Input: tokens = ["2","1","+","3","*"]
+// Output: 9
+// Explanation: ((2 + 1) * 3) = 9
+// Example 2:
+
+// Input: tokens = ["4","13","5","/","+"]
+// Output: 6
+// Explanation: (4 + (13 / 5)) = 6
+// Example 3:
+
+// Input: tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+// Output: 22
+// Explanation: ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+// = ((10 * (6 / (12 * -11))) + 17) + 5
+// = ((10 * (6 / -132)) + 17) + 5
+// = ((10 * 0) + 17) + 5
+// = (0 + 17) + 5
+// = 17 + 5
+// = 22
+//  
+
+// Constraints:
+
+// 1 <= tokens.length <= 104
+// tokens[i] is either an operator: "+", "-", "*", or "/", or an integer in the range [-200, 200].
+
+// 方法一：栈
+
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+
+    if (tokens.length === 1) return tokens;
+
+    const stack = [];
+
+    while (tokens.length) {
+        let curr = tokens.shift();
+
+        if (curr !== '+' && curr !== '-' && curr !== '*' && curr !== '/') {
+            stack.push(curr)
+        } else {
+            let last = Number(stack.pop())
+            let first = Number(stack.pop())
+
+            switch (curr) {
+                case '+':
+                    stack.push(Number(first + last));
+                    break;
+                case '-':
+                    stack.push(Number(first - last))
+                    break;
+                case '*':
+                    stack.push(Number(first * last));
+                    break;
+                default:
+                    stack.push(parseInt(first / last))
+                    break;
+            }
+        }
+    }
+
+    return stack[0]
+
+};
