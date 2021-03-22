d56ec59fb13e4e28acf4e424ce2a9ca49bf362cf
shanejix <shanejix@hotmail.com>
2021-03-22 17:54:51 +0800

    ⚡️ update

--- src/leetcode/bit-manipulation/0191.Numberof1Bits/solution.js
+++ src/leetcode/bit-manipulation/0191.Numberof1Bits/solution.js
@@ -70,3 +70,21 @@ var hammingWeight = function (n) {
 
     return count;
 };
+
+// 方法二：位运算优化
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+
+    let count = 0;
+
+    while (n) {
+        n = n & (n - 1);
+        count++;
+    }
+
+    return count;
+};
