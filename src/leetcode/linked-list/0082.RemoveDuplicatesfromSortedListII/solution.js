0b7fbb82ab5cf2e52c49193e32920509cfff4c6d
shanejix <shanejix@hotmail.com>
2021-03-25 20:52:49 +0800

    ⚡️ update

--- src/leetcode/linked-list/0082.RemoveDuplicatesfromSortedListII/solution.js
+++ src/leetcode/linked-list/0082.RemoveDuplicatesfromSortedListII/solution.js
@@ -54,3 +54,35 @@ var deleteDuplicates = function (head) {
     return dummy.next;
 
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+// 方法二：递归
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+    if (!head || !head.next) return head
+
+    if (head.val !== head.next.val) {
+        head.next = deleteDuplicates(head.next)
+    } else {
+        let move = head.next;
+
+        while (move && head.val === move.val) {
+            move = move.next
+        }
+
+        return deleteDuplicates(move);
+    }
+
+    return head;
+};
