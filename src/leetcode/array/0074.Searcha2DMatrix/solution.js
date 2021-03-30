4519d77cc97ceaf4f23d171efc5383b572da1aa5
shanejix <shanejix@hotmail.com>
2021-03-30 21:11:47 +0800

    ⚡️ update

--- src/leetcode/array/0074.Searcha2DMatrix/solution.js
+++ src/leetcode/array/0074.Searcha2DMatrix/solution.js
@@ -53,3 +53,45 @@ var searchMatrix = function (matrix, target) {
     return isExit
 };
 
+// 方法二：两次二分
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function (matrix, target) {
+    const rowIndex = binarySearchFirstColumn(matrix, target);
+    if (rowIndex < 0) {
+        return false;
+    }
+    return binarySearchRow(matrix[rowIndex], target);
+};
+
+const binarySearchFirstColumn = (matrix, target) => {
+    let low = -1, high = matrix.length - 1;
+    while (low < high) {
+        const mid = Math.floor((high - low + 1) / 2) + low;
+        if (matrix[mid][0] <= target) {
+            low = mid;
+        } else {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
+
+const binarySearchRow = (row, target) => {
+    let low = 0, high = row.length - 1;
+    while (low <= high) {
+        const mid = Math.floor((high - low) / 2) + low;
+        if (row[mid] == target) {
+            return true;
+        } else if (row[mid] > target) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return false;
+}
