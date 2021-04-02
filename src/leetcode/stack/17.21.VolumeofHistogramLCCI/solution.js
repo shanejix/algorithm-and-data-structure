40f7af9c4c627f14d5c2115401525af85b03960f
shanejix <shanejix@hotmail.com>
2021-04-02 21:04:31 +0800

    ⚡️ update

--- src/leetcode/stack/17.21.VolumeofHistogramLCCI/solution.js
+++ src/leetcode/stack/17.21.VolumeofHistogramLCCI/solution.js
@@ -49,3 +49,35 @@ var trap = function (height) {
 
     return res
 };
+
+// 方法二：预处理 + xx
+
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function (height) {
+    let len = height.length;
+
+    if (len === 0) return 0
+
+    const leftMax = new Array(len).fill(0);
+    leftMax[0] = height[0];
+    for (let i = 1; i < len; i++) {
+        leftMax[i] = Math.max(leftMax[i - 1], height[i])
+    }
+
+    const rightMax = new Array(len).fill(0);
+    rightMax[len - 1] = height[len - 1];
+    for (let i = len - 2; i >= 0; i--) {
+        rightMax[i] = Math.max(height[i], rightMax[i + 1])
+    }
+
+    let res = 0;
+    for (let i = 0; i < len; i++) {
+        res += Math.min(leftMax[i], rightMax[i]) - height[i]
+    }
+
+    return res;
+};
