d67c617a75e7d58ae154db0afcb167cebcc3bca8
shanejix <shanejix@hotmail.com>
2021-04-27 19:12:37 +0800

    📝 update 0938

---
+++ src/leetcode/binary-tree/0938.RangeSumofBST/solution.js
@@ -0,0 +1,63 @@
+// Given the root node of a binary search tree, return the sum of values of all nodes with a value in the range [low, high].
+
+//  
+
+// Example 1:
+
+
+// Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+// Output: 32
+// Example 2:
+
+
+// Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+// Output: 23
+//  
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [1, 2 * 104].
+// 1 <= Node.val <= 105
+// 1 <= low <= high <= 105
+// All Node.val are unique.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+// 方法一：递归 - 深度优先遍历
+
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function (root, low, high) {
+
+    let res = 0;
+
+    function inorder(root) {
+
+        if (!root) {
+            return
+        }
+
+        inorder(root.left);
+
+        if (root.val >= low && root.val <= high) {
+            res += root.val
+        }
+
+        inorder(root.right)
+    }
+
+    inorder(root);
+
+    return res
+};
