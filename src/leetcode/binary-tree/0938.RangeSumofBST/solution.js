124728bb372fccbd2029b73c6e2d2e05c76445f9
shanejix <shanejix@hotmail.com>
2021-04-27 19:41:42 +0800

    ⚡️ update

--- src/leetcode/binary-tree/0938.RangeSumofBST/solution.js
+++ src/leetcode/binary-tree/0938.RangeSumofBST/solution.js
@@ -64,7 +64,34 @@ var rangeSumBST = function (root, low, high) {
     return res
 };
 
-// 方法一：迭代 - 广度优先遍历 
+// 方法一：递归 - 深度优先遍历 - 优化
+
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function (root, low, high) {
+
+    if (!root) {
+        // return
+        return 0
+    }
+
+    if (root.val < low) {
+        return rangeSumBST(root.right, low, high)
+    }
+
+    if (root.val > high) {
+        return rangeSumBST(root.left, low, high)
+    }
+
+
+    return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high)
+};
+
+// 方法二：迭代 - 广度优先遍历 
 
 /**
  * @param {TreeNode} root
