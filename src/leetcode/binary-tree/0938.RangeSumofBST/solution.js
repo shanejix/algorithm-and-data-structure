7c959e81c3d32b587f4409389a474fbbb020eb27
shanejix <shanejix@hotmail.com>
2021-04-27 19:49:01 +0800

    📝 update

--- src/leetcode/binary-tree/0938.RangeSumofBST/solution.js
+++ src/leetcode/binary-tree/0938.RangeSumofBST/solution.js
@@ -1,7 +1,5 @@
 // Given the root node of a binary search tree, return the sum of values of all nodes with a value in the range [low, high].
 
-const { default: sum } = require("../../../sum");
-
 //  
 
 // Example 1:
