70e7186c24a0e6e2d8625b30fb0abf5b3c48bde3
shanejix <shanejix@hotmail.com>
2021-04-28 18:09:18 +0800

    📝 update 0633

---
+++ src/leetcode/math/0633.SumofSquareNumbers/solution.js
@@ -0,0 +1,49 @@
+// Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+
+//  
+
+// Example 1:
+
+// Input: c = 5
+// Output: true
+// Explanation: 1 * 1 + 2 * 2 = 5
+// Example 2:
+
+// Input: c = 3
+// Output: false
+// Example 3:
+
+// Input: c = 4
+// Output: true
+// Example 4:
+
+// Input: c = 2
+// Output: true
+// Example 5:
+
+// Input: c = 1
+// Output: true
+//  
+
+// Constraints:
+
+// 0 <= c <= 231 - 1
+
+// 方法一：暴力，求和转化为求差
+
+/**
+ * @param {number} c
+ * @return {boolean}
+ */
+var judgeSquareSum = function (c) {
+
+    for (let a = 0; a ** 2 < c; a++) {
+        let b = Math.sqrt(c - a ** 2);
+
+        if (b === parseInt(b)) {
+            return true
+        }
+    }
+
+    return false
+};
