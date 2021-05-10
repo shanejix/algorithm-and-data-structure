15c1c290d66e53432f870dbb94718153172faf9c
shanejix <shanejix@hotmail.com>
2021-05-10 21:09:13 +0800

    ⚡️ update

--- src/leetcode/string/0013.RomantoInteger/solution.js
+++ src/leetcode/string/0013.RomantoInteger/solution.js
@@ -134,3 +134,44 @@ function singleToIntger(roman) {
             return 1000;
     }
 }
+
+
+// 方法一：直接法 + 优化
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    let sum = 0;
+    s = s.split('');
+
+    let preNum = getValue(s[0]);
+    for (let i = 1; i < s.length; i++) {
+        let num = getValue(s[i]);
+
+        // 小值放在大值的左边，就是做减法，否则为加法
+        if (preNum < num) {
+            sum -= preNum;
+        } else {
+            sum += preNum;
+        }
+        preNum = num;
+    }
+    sum += preNum;
+    return sum;
+}
+
+
+function getValue(ch) {
+    switch (ch) {
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+}
