9192dc69fc9cb2fe9d3231d8b565f47a93670436
shanejix <shanejix@hotmail.com>
2021-05-14 22:20:07 +0800

    ⚡️ update

--- src/leetcode/string/0012.IntegertoRoman/solution.js
+++ src/leetcode/string/0012.IntegertoRoman/solution.js
@@ -159,3 +159,25 @@ var intToRoman = function (num) {
 
     return roman.join('');
 }
+
+
+// 方法一：码表 优化
+
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+    const thousands = ["", "M", "MM", "MMM"];
+    const hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+    const tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+    const ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+
+    const roman = [];
+    roman.push(thousands[Math.floor(num / 1000)]);
+    roman.push(hundreds[Math.floor(num % 1000 / 100)]);
+    roman.push(tens[Math.floor(num % 100 / 10)]);
+    roman.push(ones[num % 10]);
+
+    return roman.join('');
+};
