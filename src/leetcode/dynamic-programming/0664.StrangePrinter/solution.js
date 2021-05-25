138b41aabb65f0747fbb49159fc9de71e758f15a
shanejix <shanejix@hotmail.com>
2021-05-25 18:11:13 +0800

    📝 udpate

--- src/leetcode/dynamic-programming/0664.StrangePrinter/solution.js
+++ src/leetcode/dynamic-programming/0664.StrangePrinter/solution.js
@@ -31,15 +31,21 @@ var strangePrinter = function (s) {
 
     const len = s.length;
 
+    // dp[i][j] 表示字符串区间 [i,j] 中需要最少的打印次数 
     const dp = new Array(len).fill(0).map(() => new Array(len).fill(0))
 
+    // 从大到小枚举，满足无后效性
     for (let i = len - 1; i >= 0; i--) {
+        // 打印一个字符串的次数为 1
         dp[i][i] = 1;
 
+        // 当字符串的长度大于等于 2 时，判断区间两端的字符是否相等 s[i] === s[j]?
         for (let j = i + 1; j < len; j++) {
             if (s[i] === s[j]) {
+                // 相等 从 子区间 转移而来
                 dp[i][j] = dp[i][j - 1];
             } else {
+                // 不相等 从 枚举所有的可能组合，取最优解
                 let min = Infinity;
                 for (let k = i; k < j; k++) {
                     min = Math.min(min, dp[i][k] + dp[k + 1][j])
