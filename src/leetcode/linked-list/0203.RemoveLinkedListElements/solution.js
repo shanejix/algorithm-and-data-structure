aec00a6983a36c32857d80abce6fb4f354395263
shanejix <shanejix@hotmail.com>
2021-06-05 11:05:58 +0800

    ⚡️ update

--- src/leetcode/linked-list/0203.RemoveLinkedListElements/solution.js
+++ src/leetcode/linked-list/0203.RemoveLinkedListElements/solution.js
@@ -139,3 +139,28 @@ var removeElements = function (head, val) {
 
     return newHead.next;
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+// 方法三：递归
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+    if (head === null) {
+        return head
+    }
+
+    head.next = removeElements(head.next, val);
+
+    return head.val === val ? head.next : head
+};
