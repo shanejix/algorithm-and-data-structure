8b8960ade70a48b8cff9165cc47b8a032f654510
shanejix <shanejix@hotmail.com>
2021-06-21 21:36:22 +0800

    🐛 fix bug

--- src/leetcode/bit-manipulation/0401.BinaryWatch/solution.js
+++ src/leetcode/bit-manipulation/0401.BinaryWatch/solution.js
@@ -40,7 +40,7 @@ var readBinaryWatch = function (turnedOn) {
     for (let h = 0; h < 12; h++) {
         for (let m = 0; m < 60; m++) {
 
-            if (h.toString(2).split('0').join('').length + m.toString(2).split('').join('').length === turnedOn) {
+            if (h.toString(2).split('0').join('').length + m.toString(2).split('0').join('').length === turnedOn) {
                 ans.push(h + ':' + (m < 10 ? '0' : '') + m)
             }
         }
