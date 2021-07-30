f0f21d941404a2be9643f09bd31120f89b67d61c
shanejix <shanejix@hotmail.com>
2021-07-30 23:45:10 +0800

    📝 update

--- pattern/breadth-first-search/0637.AverageofLevelsinBinaryTree.js
+++ pattern/breadth-first-search/0637.AverageofLevelsinBinaryTree.js
@@ -1,29 +1,27 @@
-// Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
-
+// Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. Answers within 10-5 of the actual answer will be accepted.
 //  
 
 // Example 1:
 
 
-// Input: root = [3,9,20,null,null,15,7]
-// Output: [[3],[9,20],[15,7]]
+// Input: root = [3,9,20,null,15,7]
+// Output: [3.00000,14.50000,11.00000]
+// Explanation: The average value of nodes on level 0 is 3, on level 1 is 14.5, and on level 2 is 11.
+// Hence return [3, 14.5, 11].
 // Example 2:
 
-// Input: root = [1]
-// Output: [[1]]
-// Example 3:
 
-// Input: root = []
-// Output: []
+// Input: root = [3,9,20,15,7]
+// Output: [3.00000,14.50000,11.00000]
 //  
 
 // Constraints:
 
-// The number of nodes in the tree is in the range [0, 2000].
-// -1000 <= Node.val <= 1000
+// The number of nodes in the tree is in the range [1, 104].
+// -231 <= Node.val <= 231 - 1
 
 // 来源：力扣（LeetCode）
-// 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+// 链接：https://leetcode-cn.com/problems/average-of-levels-in-binary-tree
 // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 /**
@@ -37,13 +35,13 @@
 
 // 🎨 方法一：二叉树的按层遍历 
 
-// 📝 思路：按层遍历，上一层遍历结束会进入下一层，用size记录每层的宽度
+// 📝 思路：同 102 题
 
 /**
  * @param {TreeNode} root
- * @return {number[][]}
+ * @return {number[]}
  */
-var levelOrder = function (root) {
+var averageOfLevels = function (root) {
 
     if (!root) {
         return []
@@ -72,7 +70,10 @@ var levelOrder = function (root) {
             size--
         }
 
-        ans.push([...level])
+        const sum = level.reduce((acc, num) => acc + num, 0)
+        const average = sum / level.length
+        ans.push(average)
+
         level.length = 0;
     }
 
