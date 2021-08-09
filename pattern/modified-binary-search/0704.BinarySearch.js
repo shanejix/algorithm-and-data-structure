fb780b0e4d633045cfd1956c42ddced51b7fbf9f
shanejix <shanejix@hotmail.com>
2021-08-09 18:29:11 +0800

    🔥 update

--- pattern/modified-binary-search/0704.BinarySearch.js
+++ pattern/modified-binary-search/0704.BinarySearch.js
@@ -29,6 +29,8 @@
 
 // 🎨 方法一：二分查找
 
+// 📝 思路：[left,right] 左闭右闭
+
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -37,7 +39,7 @@
 var search = function (nums, target) {
 
     let left = 0;
-    let right = nums.length - 1;
+    let right = nums.length - 1; // 🔥
 
     while (left <= right) {
         let mid = left + right >> 1;
@@ -45,7 +47,36 @@ var search = function (nums, target) {
         if (nums[mid] === target) {
             return mid;
         } else if (nums[mid] > target) {
-            right = mid - 1
+            right = mid - 1; // 🔥
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        }
+    }
+
+    return -1
+};
+
+// 🎨 方法一：二分查找
+
+// 📝 思路：[left,right) 左闭右开
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+
+    let left = 0;
+    let right = nums.length; // 🔥
+
+    while (left < right) {
+        let mid = left + right >> 1;
+
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            right = mid; // 🔥
         } else if (nums[mid] < target) {
             left = mid + 1;
         }
