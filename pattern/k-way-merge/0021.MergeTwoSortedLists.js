4b78a5a709854f48298404d18db1d7a88236b65c
shanejix <shanejix@hotmail.com>
2021-08-11 20:22:42 +0800

    📝 update

--- pattern/k-way-merge/0021.MergeTwoSortedLists.js
+++ pattern/k-way-merge/0021.MergeTwoSortedLists.js
@@ -80,3 +80,37 @@ var mergeTwoLists = function (l1, l2) {
 
   return head.next
 };
+
+
+// 🎨 方法二：迭代
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  let head = tail = new ListNode(null);
+
+  while (l1 && l2) {
+    if (l1.val <= l2.val) {
+      tail.next = l1;
+      l1 = l1.next;
+    } else {
+      tail.next = l2
+      l2 = l2.next;
+    }
+    tail = tail.next;
+  }
+
+  tail.next = l1 || l2
+
+  return head.next
+};
