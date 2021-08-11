466d0b4ea56277e387288a0380364261a8c70a98
shanejix <shanejix@hotmail.com>
2021-08-11 18:11:59 +0800

    📝 update

---
+++ pattern/top-k-elements/0215.KthLargestElementinanArray.js
@@ -0,0 +1,24 @@
+// Given an integer array nums and an integer k, return the kth largest element in the array.
+
+// Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+//  
+
+// Example 1:
+
+// Input: nums = [3,2,1,5,6,4], k = 2
+// Output: 5
+// Example 2:
+
+// Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+// Output: 4
+//  
+
+// Constraints:
+
+// 1 <= k <= nums.length <= 104
+// -104 <= nums[i] <= 104
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/kth-largest-element-in-an-array
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
