1f59ca8fd3966539db364c5b996f999e1e35d149
shanejix <shanejix@hotmail.com>
2021-08-11 20:04:33 +0800

    📝 update

--- pattern/k-way-merge/0021.MergeTwoSortedLists.js
+++ pattern/k-way-merge/0021.MergeTwoSortedLists.js
@@ -63,5 +63,20 @@ var mergeTwoLists = function (l1, l2) {
  * @return {ListNode}
  */
 var mergeTwoLists = function (l1, l2) {
+  let head = tail = new ListNode(null);
 
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      tail.next = new ListNode(l1.val);
+      l1 = l1.next;
+    } else {
+      tail.next = new ListNode(l2.val)
+      l2 = l2.next;
+    }
+    tail = tail.next
+  }
+
+  tail.next = l1 || l2
+
+  return head.next
 };
