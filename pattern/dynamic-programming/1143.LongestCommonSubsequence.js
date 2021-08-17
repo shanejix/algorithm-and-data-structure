869d9355742ba6c518c16a1b5480d6ab29aacaaf
shanejix <shanejix@hotmail.com>
2021-08-17 23:43:37 +0800

    📝 update

---
+++ pattern/dynamic-programming/1143.LongestCommonSubsequence.js
@@ -0,0 +1,81 @@
+// Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+// A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+// For example, "ace" is a subsequence of "abcde".
+// A common subsequence of two strings is a subsequence that is common to both strings.
+
+//  
+
+// Example 1:
+
+// Input: text1 = "abcde", text2 = "ace" 
+// Output: 3  
+// Explanation: The longest common subsequence is "ace" and its length is 3.
+// Example 2:
+
+// Input: text1 = "abc", text2 = "abc"
+// Output: 3
+// Explanation: The longest common subsequence is "abc" and its length is 3.
+// Example 3:
+
+// Input: text1 = "abc", text2 = "def"
+// Output: 0
+// Explanation: There is no such common subsequence, so the result is 0.
+//  
+
+// Constraints:
+
+// 1 <= text1.length, text2.length <= 1000
+// text1 and text2 consist of only lowercase English characters.
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/longest-common-subsequence
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+// 🎨 方法一：动态规划
+
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+var longestCommonSubsequence = function (text1, text2) {
+    let n = text1.length;
+    let m = text2.length;
+
+    if (n * m === 0) {
+        return 0;
+    }
+
+    console.log(n, m);
+
+    const dp = new Array(n + 1).fill(0).map(() => new Array(m + 1).fill(0));
+
+    console.log(dp)
+
+    for (let i = 0; i < n + 1; i++) {
+        dp[i][0] = 0;
+    }
+
+    for (let i = 0; i < m + 1; i++) {
+        dp[0][i] = 0;
+    }
+
+    for (let i = 1; i < n + 1; i++) {
+        for (let j = 1; j < m + 1; j++) {
+
+            console.log(text1[i - 1], text2[j - 1]);
+
+            if (text1[i - 1] === text2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    console.log(dp)
+
+    return dp[n][m];
+};
