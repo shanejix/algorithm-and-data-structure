4e6d3ca4a66619dd4d2d6c42d05df30e018fc9af
shanejix <shanejix@hotmail.com>
2021-08-17 22:12:48 +0800

    📝 update

--- pattern/dynamic-programming/0005.LongestPalindromicSubstring.js
+++ pattern/dynamic-programming/0005.LongestPalindromicSubstring.js
@@ -30,7 +30,7 @@
 // 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
 // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
-// 🎨 方法一：动态规划
+// 🎨 方法一：动态规划 - 求回文子串数
 
 /**
  * @param {string} s
@@ -57,10 +57,57 @@ var longestPalindrome = function (s) {
     for (let j = 1; j < n; j++) {
 
         for (let i = 0; i < j; i++) {
+            // 子串 [i,j]
             dp[i][j] = s[i] === s[j] && (j - i < 3 || dp[i + 1][j - 1])
 
             if (dp[i][j]) {
                 ans++
+            }
+        }
+    }
+
+    return ans
+};
+
+// 🎨 方法一：动态规划 - 求最长回文子串
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+    let n = s.length;
+
+    if (n === 0) {
+        return ''
+    }
+
+    let ans = '';
+
+    // 定义dp[i][j]的值表示子串[i,j]是否为回文，并初始化 
+    const dp = new Array(n).fill(false).map(() => new Array(n).fill(false))
+
+    // 自身是回文字符串
+    for (let i = 0; i < n; i++) {
+        dp[i][i] = true;
+        ans++
+    }
+
+    for (let l = 0; j < n; l++) {
+
+        for (let i = 0; i + l < n; i++) {
+            let j = i + l
+
+            if (l === 0) {
+                dp[i][j] = true;
+            } else if (l === 1) {
+                dp[i][j] = s[i] === s[j];
+            } else {
+                dp[i][j] = s[i] === s[j] && dp[i + 1][j - 1];
+            }
+
+            if (dp[i][j] && l + 1 > res.length) {
+                res = s.slice(i, i + l + 1)
             }
         }
     }
